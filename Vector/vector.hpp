e3bc231cf82d4a89b6abf34a678a45a6fa134180
Victor MOREAU <vmoreau@e1r8p20.42.fr>
2021-09-02 13:44:19 +0200

    wtf

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -6,12 +6,12 @@
 /*   By: vmoreau <vmoreau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 13:21:46 by vmoreau           #+#    #+#             */
-/*   Updated: 2021/09/02 13:29:24 by vmoreau          ###   ########.fr       */
+/*   Updated: 2021/09/02 13:44:02 by vmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 namespace ft
-{
+{ otototototo
 	class vector
 	{
 	private:
